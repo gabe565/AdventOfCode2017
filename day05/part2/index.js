0b8eaf93596e22d5977ad3379910ea77150dc624
Gabe Cook <gabe565@users.noreply.github.com>
2017-12-05 12:52:41 -0600

    Add day 5

---
+++ day05/part2/index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+'use strict'
+
+const _  = require('lodash')
+const fs = require('fs')
+
+const filepath = '../input.txt'
+
+function fromFile(path) {
+    let data = fs.readFileSync(path, 'utf8')
+    let split = _.filter(_.split(data, /\r?\n/), _.size)
+    let memory = _.map(split, _.toInteger)
+    return memory
+}
+
+function calc(memory) {
+    let size = _.size(memory)
+    let loc = 0
+    let pc = 0
+
+    while (loc < size) {
+        let last = loc
+        loc += memory[loc]
+        if (memory[last] >= 3)
+            --memory[last]
+        else 
+            ++memory[last]
+        ++pc
+    }
+    console.log('  Program Counter: ' + pc)
+}
+
+console.log('Test')
+calc([0, 3, 0, 1, -3])
+
+console.log('Input')
+let data = fs.readFileSync(filepath, 'utf8')
+let memory = fromFile(filepath)
+calc(memory)
