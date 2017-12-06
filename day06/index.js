0ddf38dff0958956e7451d10fc61595f293a3039
Gabe Cook <gabe565@users.noreply.github.com>
2017-12-06 10:43:40 -0600

    Add day 6

---
+++ day06/index.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+'use strict'
+
+const fs = require('fs')
+
+function calc(input) {
+    let memory  = input.split(/\s+/).map(Number)
+    console.log(memory.toString())
+    let history = new Map()
+    let size = memory.length
+    let pc = 0
+
+    while (!history.has(memory.toString())) {
+        history.set(memory.toString(), pc)
+        let val = Math.max(...memory)
+        let key = memory.indexOf(val)
+        memory[key] = 0
+        let curr = (key + 1) % size
+        while (val > 0) {
+            ++memory[curr]
+            --val
+            curr = (curr + 1) % size
+        }
+        ++pc
+    }
+    let first = history.get(memory.toString())
+    console.log('Count : ' + pc)
+    console.log('Cycles: ' + (pc - first))
+}
+
+console.log('Tests')
+calc('0 2 7 0')
+console.log('\nInput')
+var data = fs.readFileSync('input.txt', 'utf8')
+calc(data.replace(/\r?\n$/, ''))
